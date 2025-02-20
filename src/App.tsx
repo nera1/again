24cf0d4b201e38e04a026d1ed512681d590199e1
nera1 <nera4936@gmail.com>
2025-02-20 23:56:58 +0900

    feat: add titles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
 import { useEffect } from "react";
-import styles from "./App.module.scss";
+
 import getData from "./util/getData";
 import Header from "./components/header";
+
+import { ThemeProvider } from "./components/theme-provider/theme-provider";
+
+import styles from "./App.module.scss";
 
 function App() {
   useEffect(() => {
     getData();
   });
+
+  const today = new Date();
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(today);
+
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Header />
-      <div className={styles["container"]}></div>
-    </>
+      <div className={styles["container"]}>
+        <h2 className="text-3xl font-bold tracking-tight">Today's</h2>
+        <p className="text-sm mt-2 text-muted-foreground">{formattedDate}</p>
+        <h2 className="text-3xl font-bold tracking-tight">Missed</h2>
+        <p className="text-sm mt-2 text-muted-foreground">
+          Skipped or unsolved problems
+        </p>
+        <h2 className="text-3xl font-bold tracking-tight">Next up</h2>
+        <p className="text-sm mt-2 text-muted-foreground">
+          Problems coming soon
+        </p>
+        <h2 className="text-3xl font-bold tracking-tight">Solved</h2>
+        <p className="text-sm mt-2 text-muted-foreground">
+          Already solved problems
+        </p>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,32 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
+import { Button } from "@/components/ui/button";
+
+import { ChevronRight, Menu } from "lucide-react";
+
+import LogoSymbol from "./icons/logo-symbol";
+
 import styles from "../styles/header.module.scss";
 
 const Header: FunctionComponent = () => {
-  return <header className={styles["header"]}></header>;
+  return (
+    <header className={styles["header"]}>
+      <div className={styles["container"]}>
+        <div className={styles["left"]}>
+          <Button className="invisible" variant="outline" size="icon">
+            <ChevronRight />
+          </Button>
+        </div>
+        <div className={styles["center"]}>
+          <LogoSymbol />
+        </div>
+        <div className={styles["right"]}>
+          <Button variant="outline" size="icon">
+            <Menu />
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
