91421a18cb5031c61501918b23266bfac8486dc6
nera1 <nera4936@gmail.com>
2025-02-21 00:00:49 +0900

    chore: remove unused thing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent } from "react";
 import { Button } from "@/components/ui/button";
 
 import { ChevronRight, Menu } from "lucide-react";
